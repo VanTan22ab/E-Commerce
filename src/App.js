7817fd8527373ff0ae7c418574c7be301fafe2da
VanTan22ab <izukumi5672@gmail.com>
2025-01-09 14:56:08 +0700

    fix bug

--- src/App.js
+++ src/App.js
@@ -6,16 +6,12 @@ import AuthForm from "./pages/Auth/AuthForm";
 function App() {
   return (
     <>
-<<<<<<< HEAD
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<AuthForm />} />
         </Routes>
       </Router>
-=======
-    <p>App will be comming soon<p>
->>>>>>> 3dd21237d37ea128b3d4540b6b5a44bd3fc1df8d
     </>
   );
 }
